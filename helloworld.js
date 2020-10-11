e6bb8eaf6948f45796b92749ae7164a734db39ef
hecjdondiego@gmail.com <hecjdondiego@gmail.com>
2020-10-11 12:08:54 -0500

    Updated hello world

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,7 @@ let http = require('http');
 
 let server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h5>Hello World!</h5>');
+  res.close('<h5>Hello World!</h5>');
 });
 
 const port = process.env.port || 8080;
